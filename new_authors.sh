2280c70c5bac4d5ef3ecfa30e004240031a08c27
John Dickinson <me@not.mn>
2014-02-23 15:21:46 -0800

    script the work for finding new authors

--- new_authors.sh
+++ new_authors.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-git diff $1 -- AUTHORS | grep '[+-]' | grep -v '@@' | grep -v AUTHORS
+HERE=`pwd`
+
+cd /Users/john/Documents/swift/
+git shortlog --no-merges -nes >${HERE}/vcs_authors
+cd ${HERE}
+python ./authors.py
