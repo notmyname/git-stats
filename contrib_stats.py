28c89fa136e992f645651ce8968eb296317290c5
John Dickinson <me@not.mn>
2014-10-31 23:30:07 -0700

    create a graph of active contributors

--- contrib_stats.py
+++ contrib_stats.py
@@ -1,6 +1,8 @@
 import subprocess
 
-chunk_size = 30  # number of days for one "chunk" of time
+from matplotlib import pyplot
+
+chunk_size = 7  # number of days for one "chunk" of time
 
 def timeblock_iter():
     start = 0
@@ -10,11 +12,37 @@ def timeblock_iter():
         start = end
         end += chunk_size
 
-for start, end in timeblock_iter():
-    cmd = "git shortlog -nes --no-merges --before='@{%d days ago}' --since='@{%d days ago}' | wc -l" % (start, end)
-    count = subprocess.check_output(cmd, shell=True)
-    count.strip()
-    count = int(count)
-    if count <= 0:
-        break
-    print '%d\t%d\t%d' % (start, end, count)
+def get_data():
+    data = []
+    zeros_found = 0
+    for start, end in timeblock_iter():
+        cmd = "git shortlog -nes --no-merges --before='@{%d days ago}' --since='@{%d days ago}' | wc -l" % (start, end)
+        count = subprocess.check_output(cmd, shell=True)
+        count.strip()
+        count = int(count)
+        if count <= 0:
+            zeros_found += 1
+        if zeros_found >= 5:  # more than 5 chunks at 0 is our escape valve
+            break
+        data.append((start, end, count))
+    return data
+
+def make_graph():
+    d = get_data()
+    d.reverse()
+    starts = [x[0] for x in d]
+    values = [x[2] for x in d]
+    pyplot.plot(starts, values, '-', drawstyle='steps-mid')  #, color='white')
+    pyplot.title('Active contributors')
+    pyplot.xlabel('Days Ago')
+    pyplot.ylabel('Contributors')
+    ax = pyplot.gca()
+    ax.invert_xaxis()
+    #ax.set_axis_bgcolor('black')
+    fig = pyplot.gcf()
+    fig.set_size_inches(16, 4)
+    fig.dpi = 200
+    fig.set_frameon(True)
+    fig.savefig('active_contribs.png', bbox_inches='tight', pad_inches=0.25)
+
+make_graph()
