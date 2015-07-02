c3fad9670284b69e9ccfc674118e9b628162437a
John Dickinson <me@not.mn>
2015-07-02 15:44:57 -0700

    rolling average on active contributors

--- contrib_stats.py
+++ contrib_stats.py
@@ -135,15 +135,12 @@ def make_graph(contribs_by_days_ago, active_window=14):
     all_contribs = set()
     totals = []
     actives = []
+    actives_avg = []
+    rolling_avg_window = 90
     dtotal = []
     dactive = []
     contributor_activity = {}  # contrib -> [(start_date, end_date), ...]
     contrib_activity_days = {}  # contrib -> [(start_days_ago, end_days_ago), ...]
-    rolling_sets = {}
-    active_sets = {}
-    for window in (14, 60, 365):
-        rolling_sets[window] = RollingSet(window)
-        active_sets[window] = []
     rs = RollingSet(active_window)
     for date, c in contribs_by_days_ago:
         end_window = datetime.datetime.strptime(date, '%Y-%m-%d') + \
@@ -178,9 +175,9 @@ def make_graph(contribs_by_days_ago, active_window=14):
         totals.append(len(all_contribs))
         rs.add(c)
         actives.append(len(rs))
-        for window in rolling_sets:
-            rolling_sets[window].add(c)
-            active_sets[window].append(len(rolling_sets[window]))
+        denom = min(len(actives), rolling_avg_window)
+        s = sum(actives[-rolling_avg_window:])
+        actives_avg.append(float(s) / denom)
         try:
             dtotal.append(totals[-1] - totals[-2])
             dactive.append(actives[-1] - actives[-2])
@@ -212,19 +209,18 @@ def make_graph(contribs_by_days_ago, active_window=14):
 
     xs = range(MAX_DAYS_AGO, MIN_DAYS_AGO, -1)
 
-    lens = map(len, [totals, actives, dtotal, dactive, xs])
+    lens = map(len, [totals, actives, actives_avg, dtotal, dactive, xs])
     assert len(set(lens)) == 1, lens
 
     title_date = (datetime.datetime.now() - datetime.timedelta(days=MIN_DAYS_AGO)).date()
 
     # graph active contribs
     lookback = MAX_DAYS_AGO
-    # pyplot.plot(xs[-lookback:], actives[-lookback:], '-', color='blue',
-    #             label="Active contributors", drawstyle="steps")
-    for window in active_sets:
-        pyplot.plot(xs[-lookback:], active_sets[window][-lookback:], '-',
-                    label="Active contributors (%s)" % window,
-                    drawstyle="steps")
+    pyplot.plot(xs[-lookback:], actives[-lookback:], '-', color='blue',
+                label="Active contributors", drawstyle="steps")
+    pyplot.plot(xs[-lookback:], actives_avg[-lookback:], '-', color='red',
+                label="Active contributors (%d day avg)" % rolling_avg_window,
+                drawstyle="steps")
     pyplot.title('Active contributors (as of %s)' % title_date)
     pyplot.xlabel('Days Ago')
     pyplot.ylabel('Contributors')
