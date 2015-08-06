6c22d9cb246d9f98fab1ebddfc79208f0fa2aafe
John Dickinson <me@not.mn>
2015-08-06 16:52:18 -0700

    added reviews to the refactored data model

--- build_swift_gerrit_history.sh
+++ build_swift_gerrit_history.sh
@@ -3,7 +3,7 @@
 ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON project:openstack/swift --comments limit:500 >swift_gerrit_history.patches
 
 
-LAST=`tail -2 swift_gerrit_history.patches | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep sortKey | cut -d: -f2 | cut -d: -f2 | sed 's/"//g'`
+LAST=`tail -20 swift_gerrit_history.patches | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep sortKey | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
 
 OLDLAST=''
 
@@ -13,7 +13,7 @@ while [[ $OLDLAST != $LAST ]]; do
 
     ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON project:openstack/swift --comments limit:500 resume_sortkey:$LAST >>swift_gerrit_history.patches
 
-    LAST=`tail -2 swift_gerrit_history.patches | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep sortKey | cut -d: -f2 | cut -d: -f2 | sed 's/"//g'`
+    LAST=`tail -20 swift_gerrit_history.patches | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep sortKey | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
 
     echo $LAST, $OLDLAST
 done
--- contrib_stats2.py
+++ contrib_stats2.py
@@ -47,16 +47,16 @@ excluded_authors = (
     'Coraid CI <coraid-openstack-ci-all@mirantis.com>',
 )
 
-def save_commits(contribs_by_days_ago, authors_by_count, filename):
-    listified = [(d, list(e)) for (d, e) in contribs_by_days_ago]
+def save_commits(contribs_by_date, authors_by_count, filename):
+    listified = [(d, list(e)) for (d, e) in contribs_by_date.items()]
     with open(filename, 'wb') as f:
         json.dump((listified, authors_by_count), f)
 
 def load_commits(filename):
     with open(filename, 'rb') as f:
         (listified, authors_by_count) = json.load(f)
-    contribs_by_days_ago = [(d, set(e)) for (d, e) in listified]
-    return contribs_by_days_ago, authors_by_count
+    contribs_by_date = {d: set(e) for (d, e) in listified}
+    return contribs_by_date, authors_by_count
 
 def get_one_day(date):
     next_day = date + datetime.timedelta(days=1)
@@ -78,22 +78,55 @@ def get_one_day(date):
     return authors, authors_by_count
 
 def get_data(start_date, end_date):
-    data = []
+    data = defaultdict(set)
     authors_by_count = defaultdict(int)
     for date in date_range(start_date, end_date, strings=False):
         authors_for_day, by_count = get_one_day(date)
         for a, c in by_count.items():
             authors_by_count[a] += c
-        data.append((date.strftime('%Y-%m-%d'), authors_for_day))
+        data[date.strftime('%Y-%m-%d')].update(authors_for_day)
         print date  # how do I print only every 20 days?
     return data, authors_by_count
+
+def load_reviewers(filename):
+    reviewers_by_date = defaultdict(set)
+    with open(filename, 'rb') as f:
+        for line in f:
+            if line:
+                review_data = json.loads(line)
+            else:
+                continue
+            if 'comments' not in review_data:
+                continue
+            for review_comment in review_data['comments']:
+                when = review_comment['timestamp']
+                when = datetime.datetime.utcfromtimestamp(when).strftime('%Y-%m-%d')
+                reviewer = review_comment['reviewer']
+                if 'email' not in reviewer:
+                    continue
+                email = '<%s>' % reviewer['email']
+                name_email = '%s %s' % (reviewer['name'], email)
+                if name_email not in excluded_authors:
+                    reviewers_by_date[when].add(name_email)
+    return reviewers_by_date
+
+def map_people(unmapped_people):
+    mapped_people = set()
+    the_people_map = {
+        #'John Dickinson <me@not.mn>': 'Yo!! <yo>'
+    }
+    for person in unmapped_people:
+        if person in the_people_map:
+            person = the_people_map[person]
+        mapped_people.add(person)
+    return mapped_people
 
 if __name__ == '__main__':
     # load patch info
     try:
-        contribs_by_days_ago, authors_by_count = load_commits(FILENAME)
+        contribs_by_date, authors_by_count = load_commits(FILENAME)
         # update the data first
-        most_recent_date = max(x[0] for x in contribs_by_days_ago)
+        most_recent_date = max(contribs_by_date.keys())
         most_recent_date = datetime.datetime.strptime(
             most_recent_date, '%Y-%m-%d').date()
         print 'Last date found in data file:', most_recent_date
@@ -101,20 +134,54 @@ if __name__ == '__main__':
         if most_recent_date < LAST_DATE:
             print 'Updating previous data with data since %s...' % most_recent_date
             recent_data, new_by_count = get_data(most_recent_date, LAST_DATE)
-            contribs_by_days_ago.extend(recent_data)
+            contribs_by_date.extend(recent_data)
             for a, c in new_by_count.items():
                 if a not in authors_by_count:
                     authors_by_count[a] = 0
                 authors_by_count[a] += c
-            save_commits(contribs_by_days_ago, authors_by_count, FILENAME)
+            save_commits(contribs_by_date, authors_by_count, FILENAME)
         else:
             print 'Data file (%s) is up to date.' % FILENAME
-    except (IOError, ValueError):
-        contribs_by_days_ago, authors_by_count = get_data(FIRST_DATE, LAST_DATE)
-        save_commits(contribs_by_days_ago, authors_by_count, FILENAME)
+    except (IOError, ValueError), exc:
+        print exc
+        contribs_by_date, authors_by_count = get_data(FIRST_DATE, LAST_DATE)
+        save_commits(contribs_by_date, authors_by_count, FILENAME)
     sorted_authors = sorted(authors_by_count.keys())
-    for a in sorted_authors:
-        print a, authors_by_count[a]
+
     # load review info
-    # combine data sources down to one set of "contributors"
+    reviewers_by_date = load_reviewers(REVIEWS_FILENAME)
+
+    # combine data sources down to one set of contributors and dates
+    people_by_date = defaultdict(dict)
+    dates_by_person = defaultdict(lambda: defaultdict(set))
+    first_contrib_date = min(contribs_by_date.keys())
+    first_review_date = min(reviewers_by_date.keys())
+    global_first_date = str(min(first_contrib_date, first_review_date))
+    last_contrib_date = max(contribs_by_date.keys())
+    last_review_date = max(reviewers_by_date.keys())
+    global_last_date = str(max(last_contrib_date, last_review_date))
+    print 'Global first date is:', global_first_date
+    print 'Global last date is:', global_last_date
+
+    for date in date_range(global_first_date, global_last_date):
+        contribs = contribs_by_date.get(date, set())
+        reviews = reviewers_by_date.get(date, set())
+        contribs = map_people(contribs)
+        reviews = map_people(reviews)
+        people_by_date[date]['contribs'] = contribs
+        people_by_date[date]['reviews'] = reviews
+        date_obj = datetime.datetime.strptime(date, '%Y-%m-%d')
+        contrib_window = datetime.timedelta(days=14)
+        review_window = datetime.timedelta(days=3)
+        for person in contribs:
+            end_date = date_obj + contrib_window
+            for d in date_range(date, end_date):
+                dates_by_person[person]['contribs'].add(date)
+        for person in reviews:
+            end_date = date_obj + review_window
+            for d in date_range(date, end_date):
+                dates_by_person[person]['reviews'].add(date)
+
+    print min(dates_by_person['John Dickinson <me@not.mn>']['contribs'])
+
     # draw graphs
