3eff00f95d14232199fb11e5ba36326e33e508ee
John Dickinson <me@not.mn>
2015-11-08 20:57:18 -0800

    added 'refresh all' script for crontab

---
+++ refresh_all_stats.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+ORIGPWD=`pwd`
+cd /Users/john/Documents/git-stats
+
+# clear out old data
+rm -f all_stars.data
+
+# get reviewer history
+./build_swift_gerrit_history.sh
+
+# get swift history and build weightings
+./swift_contrib_stats.sh
+
+# rebuild "most starred" list
+./get_stars.sh
+
+cd ${ORIGPWD}
