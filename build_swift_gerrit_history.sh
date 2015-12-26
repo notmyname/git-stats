dceae3eda46bba6fe0e29f68863c3d65fc4fb212
John Dickinson <me@not.mn>
2015-12-26 13:00:33 -0800

    new gerrit, new query syntax

--- build_swift_gerrit_history.sh
+++ build_swift_gerrit_history.sh
@@ -1,37 +1,42 @@
 #/bin/bash
 
+PAGE_SIZE=500
+
 # grab comments
 PATCHES="swift_gerrit_history.patches"
-QUERY="project:openstack/swift branch:master --comments"
+QUERY="project:openstack/swift branch:master limit:$PAGE_SIZE --comments --all-reviewers"
 ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY >$PATCHES
-LAST=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep sortKey | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
-OLDLAST=''
-while [[ $OLDLAST != $LAST ]]; do
-    OLDLAST=$LAST
-    ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY resume_sortkey:$LAST >>$PATCHES
-    LAST=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep sortKey | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
+MORE=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep moreChanges | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
+I=1
+while [[ $MORE == "true" ]]; do
+    START=$((I * PAGE_SIZE))
+    ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY --start=$START >>$PATCHES
+    MORE=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep moreChanges | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
+    I=$((I+1))
 done
 
 # grab open patches (that aren't WIP)
 PATCHES="swift-open.patches"
-QUERY="project:openstack/swift branch:master status:open NOT label:Workflow\<=-1"
+QUERY="project:openstack/swift branch:master status:open NOT label:Workflow\<=-1 limit:$PAGE_SIZE"
 ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY >$PATCHES
-LAST=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep sortKey | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
-OLDLAST=''
-while [[ $OLDLAST != $LAST ]]; do
-    OLDLAST=$LAST
-    ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY resume_sortkey:$LAST >>$PATCHES
-    LAST=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep sortKey | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
+MORE=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep moreChanges | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
+I=1
+while [[ $MORE == "true" ]]; do
+    START=$((I * PAGE_SIZE))
+    ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY --start=$START >>$PATCHES
+    MORE=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep moreChanges | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
+    I=$((I+1))
 done
 
 # grab closed patches
 PATCHES="swift-closed.patches"
-QUERY="project:openstack/swift branch:master status:closed NOT label:Workflow\<=-1"
+QUERY="project:openstack/swift branch:master status:closed NOT label:Workflow\<=-1 limit:$PAGE_SIZE"
 ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY >$PATCHES
-LAST=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep sortKey | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
-OLDLAST=''
-while [[ $OLDLAST != $LAST ]]; do
-    OLDLAST=$LAST
-    ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY resume_sortkey:$LAST >>$PATCHES
-    LAST=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep sortKey | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
+MORE=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep moreChanges | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
+I=1
+while [[ $MORE == "true" ]]; do
+    START=$((I * PAGE_SIZE))
+    ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY --start=$START >>$PATCHES
+    MORE=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep moreChanges | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
+    I=$((I+1))
 done
