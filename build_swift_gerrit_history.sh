af03aeb7cb288e3ac49a84b0440e9d75597e03f6
John Dickinson <me@not.mn>
2016-02-10 16:35:40 -0800

    added is:mergable filter to gerrit data

--- build_swift_gerrit_history.sh
+++ build_swift_gerrit_history.sh
@@ -4,7 +4,7 @@ PAGE_SIZE=500
 
 # grab comments
 PATCHES="swift_gerrit_history.patches"
-QUERY="project:openstack/swift branch:master limit:$PAGE_SIZE --comments --all-reviewers"
+QUERY="project:openstack/swift branch:master limit:$PAGE_SIZE --comments --all-reviewers is:mergeable"
 ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY >$PATCHES
 MORE=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep moreChanges | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
 I=1
@@ -17,7 +17,7 @@ done
 
 # grab open patches (that aren't WIP)
 PATCHES="swift-open.patches"
-QUERY="project:openstack/swift branch:master status:open NOT label:Workflow\<=-1 limit:$PAGE_SIZE"
+QUERY="project:openstack/swift branch:master status:open NOT label:Workflow\<=-1 limit:$PAGE_SIZE is:mergeable"
 ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY >$PATCHES
 MORE=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep moreChanges | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
 I=1
@@ -30,7 +30,7 @@ done
 
 # comments on open patches that aren't WIP
 PATCHES="swift-open-comments.patches"
-QUERY="project:openstack/swift branch:master status:open NOT label:Workflow\<=-1 limit:$PAGE_SIZE --comments --all-reviewers"
+QUERY="project:openstack/swift branch:master status:open NOT label:Workflow\<=-1 limit:$PAGE_SIZE --comments --all-reviewers is:mergeable"
 ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY >$PATCHES
 MORE=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep moreChanges | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
 I=1
@@ -43,7 +43,7 @@ done
 
 # grab closed patches
 PATCHES="swift-closed.patches"
-QUERY="project:openstack/swift branch:master status:closed NOT label:Workflow\<=-1 limit:$PAGE_SIZE"
+QUERY="project:openstack/swift branch:master status:closed NOT label:Workflow\<=-1 limit:$PAGE_SIZE is:mergeable"
 ssh -p29418 notmyname@review.openstack.org gerrit query --format JSON $QUERY >$PATCHES
 MORE=`tail -20 $PATCHES | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep moreChanges | cut -d: -f2 | cut -d: -f2 | sed 's/"//g' | tail -1`
 I=1
